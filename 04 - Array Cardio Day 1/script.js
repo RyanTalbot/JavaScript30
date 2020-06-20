8ed9a16df3495e69093af90ee4f25c001e206376
Ryan Talbot <ryan-talbot@outlook.com>
2020-06-20 17:38:42 +0100

    Second exercise complete

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -81,6 +81,10 @@ console.table(fifteenHunArrow);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventor first and last names
+const fullName = inventors.map(
+  (inventor) => `${inventor.first} ${inventor.last}`
+);
+console.log(fullName);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
