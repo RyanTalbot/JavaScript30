34c4c2c30d55e903dba08f057ef350d1fcced2ab
Ryan Talbot <ryan-talbot@outlook.com>
2020-07-24 23:06:26 +0100

    addItem function

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -1,3 +1,17 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = JSON.parse(localStorage.getItem("items")) || [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector("[name=item]").value;
+  const item = {
+    text,
+    done: false,
+  };
+
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
+  this.reset();
+}
