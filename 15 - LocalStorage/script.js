2f091af388b158c3ae2dfa58aaa98b62b073ffa5
Ryan Talbot <ryan-talbot@outlook.com>
2020-07-24 23:07:27 +0100

    toggleDone function

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -30,3 +30,12 @@ function populateList(plates = [], platesList) {
     })
     .join("");
 }
+
+function toggleDone(e) {
+  if (!e.target.matches("input")) return; // skip this unless it's an input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
